e4381a0043f3f0e334104149ec3d1d575faa43a7
Dvir <dvir199710@gmail.com>
2022-03-13 15:29:23 +0200

    final

--- mat.cpp
+++ mat.cpp
@@ -1,17 +1,13 @@
 #include "mat.hpp"
 #include <string>
 #include <iostream>
-// #include <algorithm>
 #include <stdexcept>
 #include <vector>
-// #include <typeinfo>
-// #include <ctype.h>
 
 using namespace std;
 namespace ariel{
-
     /*
-    that func get 2 num and 2 symbol שnd if indeed meets the conditions 
+    that func get 2 num and 2 symbol and if indeed meets the conditions 
     the func return string(mat with the two symbol).
     */
     string mat(int col, int row, char s1, char s2)
@@ -19,36 +15,19 @@ namespace ariel{
         vector<vector<int>> matrix(row, vector<int>(col));
         int const minChar = 33;
         int const maxChar = 126;
-        int number = 1;
         if (row <= 0 || col <= 0)
         {
-            throw runtime_error("Mat size is unegative or zero");
+            throw runtime_error("Mat size is unegative or zero");// check if row or col is negative or zero
         }
         if (row % 2 == 0 || col % 2 == 0)
         {
-            throw runtime_error("Mat size is always odd");
+            throw runtime_error("Mat size is always odd");// check if row or col is even I accept just odd
         }
-        // if (typeid(col).name() != typeid(number).name() || typeid(row).name() != typeid(number).name())
-        // {
-        //     throw runtime_error("problame with the type of int");
-        // }
-        if (!(s1 >= minChar && s1 <= maxChar))
-        {
-            throw runtime_error("unvalid symbol");
-        }
-        if (!(s2 >= minChar && s2 <= maxChar))
+        if (!(s1 >= minChar && s1 <= maxChar) || !(s2 >= minChar && s2 <= maxChar)) // check if s1 or s2 is unvalid symbol from the ascii table
         {
             throw runtime_error("unvalid symbol");
         }
         string outMat;
-        // int **myMat1 = new int *[row];
-        // int **myMat2 = new int *[row];
-        // for (int i = 0; i < row; ++i)
-        // {
-        //     myMat1[i] = new int[col];
-        //     myMat2[i] = new int[col];
-
-        // }
         vector<vector<int>> myMat1(row, vector<int>(col));
         vector<vector<int>> myMat2(row, vector<int>(col));
         for (int i = 0; i < row; i++)
@@ -58,29 +37,8 @@ namespace ariel{
                 myMat1[i][j] = min(i, j);
                 myMat2[i][j] = min(row - i - 1, col - j - 1);
                 myMat1[i][j] = min(myMat1[i][j], myMat2[i][j]);
-                // cout << myMat1[i][j];
             }
-            // cout << endl;
         }
-        // for (int i = 0; i < row; i++)
-        // {
-        //     for (int j = 0; j < col; j++)
-        //     {
-        //         myMat2[i][j] = min(row - i - 1, col - j - 1);
-        //         cout<<myMat2[i][j];
-        //     }
-        //     cout<<endl;
-        // }
-        // cout << "end\n";
-        // for (int i = 0; i < row; i++)
-        // {
-        //     for (int j = 0; j < col; j++)
-        //     {
-        //         myMat1[i][j] = min(myMat1[i][j], myMat2[i][j]);
-        //         cout<<myMat1[i][j];
-        //     }
-        //     cout<<endl;
-        // }
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
@@ -99,15 +57,6 @@ namespace ariel{
                 outMat += '\n';
             }
         }
-        // for (int i = 0; i < row; ++i)
-        // {
-        //     delete[] myMat1[i];
-        //     delete[] myMat2[i];
-        // }
-        // delete[] myMat1;
-        // delete[] myMat2;
-        // cout << "out mat:\n"
-        //      << outMat;
         return outMat;
     }
 }
